8654e3074aba23cd2719d028cae9bec5b581b369
Matteo Loporchio <mloporchio@users.noreply.github.com>
2026-04-14 14:29:31 +0000

    OracleQueue interface modification

--- chain/contracts/IOracleQueue.sol
+++ chain/contracts/IOracleQueue.sol
@@ -5,14 +5,5 @@ interface IOracleQueue {
     function setAggregator(address _aggregatorAddress) external;
     function requestAttribution(string calldata _ipfsCid, address sender, uint256 value) external;
     function approveJob(uint256 _requestId) external;
-    function transmit(
-        bytes32 configDigest,
-        uint64 seqNr,
-        bytes calldata report,
-        bytes32[] calldata rs, 
-        bytes32[] calldata ss, 
-        bytes32 rawVs
-    ) external;
-    function rewardOracle(address payable _oracle) external;
 }
 
